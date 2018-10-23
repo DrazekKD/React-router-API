a0026af2cda58ff7dfb292c8edb9bd4e29e5a454
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2018-10-23 08:08:42 +0200

    start clean react Sass + Typescript

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import './App.scss';
+import './App.css';
 
-import Blog from './containers/Blog/Blog'
+import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-          <Blog/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </div>
     );
   }
