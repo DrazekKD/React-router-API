a96731bf13d71875b337dc9f23a796f4ce50e886
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2018-10-29 09:32:06 +0100

    add clean start project React The Net Ninja

---
+++ my-app/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks'
+
+const Navbar = () => {
+	return(
+		<nav>
+
+			</nav>
+	)
+};
+
+export default Navbar;
