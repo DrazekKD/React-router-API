bb02e4f25f36773f3bd11ece9ba55c2069f20b00
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2018-10-30 14:42:07 +0100

    SignUp component add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import NavbarTop from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SingIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
                 <Route exact path='/' component={Dashboard}/>
                 <Route path='/project/:id' component={ProjectDetails}/>
                 <Route path='/signIn' component={SignIn}/>
+                <Route path='/signUp' component={SignUp}/>
             </Switch>
         </div>
       </BrowserRouter>
